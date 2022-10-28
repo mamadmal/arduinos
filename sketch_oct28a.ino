13ce138086a9ddd43e50a4b09b51461bd0e00350
Muhammad <76088971+mamadmal@users.noreply.github.com>
2022-10-28 18:32:55 +0330

    Add files via upload
    arduino program

---
+++ sketch_oct28a.ino
@@ -0,0 +1,107 @@
+void setup() 
+{
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(13, 1);
+  delay(5000);
+  digitalWrite(13, 0);
+  delay(5000);
+
+  digitalWrite(13, 1);
+  delay(4500);
+  digitalWrite(13, 0);
+  delay(4500);
+
+  digitalWrite(13, 1);
+  delay(4000);
+  digitalWrite(13, 0);
+  delay(4000);
+
+  digitalWrite(13, 1);
+  delay(3500);
+  digitalWrite(13, 0);
+  delay(3500);
+
+  digitalWrite(13, 1);
+  delay(3000);
+  digitalWrite(13, 0);
+  delay(3000);
+
+  digitalWrite(13, 1);
+  delay(2000);
+  digitalWrite(13, 0);
+  delay(2000);
+
+  digitalWrite(13, 1);
+  delay(1500);
+  digitalWrite(13, 0);
+  delay(1500);
+
+  digitalWrite(13, 1);
+  delay(1000);
+  digitalWrite(13, 0);
+  delay(1000);
+
+  digitalWrite(13, 1);
+  delay(500);
+  digitalWrite(13, 0);
+  delay(500);
+
+  digitalWrite(13, 1);
+  delay(250);
+  digitalWrite(13, 0);
+  delay(250);
+
+  digitalWrite(13, 1);
+  delay(125);
+  digitalWrite(13, 0);
+  delay(125);
+
+  digitalWrite(13, 1);
+  delay(75);
+  digitalWrite(13, 0);
+  delay(75);
+
+  digitalWrite(13, 1);
+  delay(75);
+  digitalWrite(13, 0);
+  delay(75);
+
+  digitalWrite(13, 1);
+  delay(75);
+  digitalWrite(13, 0);
+  delay(50);
+
+  digitalWrite(13, 1);
+  delay(45);
+  digitalWrite(13, 0);
+  delay(45);
+
+  digitalWrite(13, 1);
+  delay(45);
+  digitalWrite(13, 0);
+  delay(45);
+
+  digitalWrite(13, 1);
+  delay(45);
+  digitalWrite(13, 0);
+  delay(45);
+
+  digitalWrite(13, 1);
+  delay(45);
+  digitalWrite(13, 0);
+  delay(45);
+
+  digitalWrite(13, 1);
+  delay(45);
+  digitalWrite(13, 0);
+  delay(45);
+
+  digitalWrite(13, 1);
+  delay(45);
+  digitalWrite(13, 0);
+  delay(45);
+
+}
